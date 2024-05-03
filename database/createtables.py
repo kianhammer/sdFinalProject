34f085ef31fa3d018b82458ce9b77ab5e545f095
DanielChen29 <daniel.yp.chen@gmail.com>
2024-05-03 13:22:46 -0500

    beggining to create database

---
+++ database/createtables.py
@@ -0,0 +1,47 @@
+import psycopg2
+
+def create_tables():
+    """ Create tables in the PostgreSQL database"""
+    commands = (
+        """
+        DROP TABLE IF EXISTS cutstats
+        """,
+        """
+        CREATE TABLE cutstats (
+            Date timestamp with timezone,
+            Opponent text,
+            Final Score text,
+            Point real,
+            Pulled text,
+            Scored text,
+            Hucks - Completed real,
+            Hucks - Incomplete - Forced real,
+            Hucks - Incomplete - Unforced real,
+            Hucks - Incomplete - Other real,
+            Endzone - Scored real,
+            Endzone - Not Scored - Forced real,
+            Endzone - Not Scored - Unforced real,
+            Endzone - Not Scored - Unknown real,
+            Blocks - Forced,Blocks - Unforced real,
+            Turnovers - Forced real,
+            Turnovers - Unforced real,
+            Players text,
+        )
+        """)
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,   
+        database="chend2",
+        user="chend2",
+        password="plad242books")
+
+    cur = conn.cursor()
+
+    for command in commands:
+        cur.execute(command)
+
+    conn.commit()
+
+if __name__ == '__main__':
+    create_tables()
