1cc402a7a2693a73186d272f89ac64ae24b52ac6
kianhammer <hammerk@carleton.edu>
2024-05-15 11:27:00 -0400

    testing new branch

--- website/pages.py
+++ website/pages.py
@@ -9,6 +9,10 @@ def welcome():
     message = f"CUT stats web page" 
     return render_template("homepage.html", someText = message)
 
+@app.route('/gameStats')
+def gameStats():
+	return render_template("homepage.html", someText = f"hello")
+
 if __name__ == '__main__':
     my_port = 5202
     app.run(host='0.0.0.0', port = my_port) 
