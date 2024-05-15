a0a20c74e11b7b5125bbda346e386334484d47cd
kianhammer <165401427+kianhammer@users.noreply.github.com>
2024-05-15 10:54:32 -0500

    Update pages.py

--- website/pages.py
+++ website/pages.py
@@ -9,8 +9,21 @@ def welcome():
     message = f"CUT stats web page" 
     return render_template("homepage.html", someText = message)
 
-@app.route('/gameStats')
-def gameStats():
+@app.route('/stats/game/<opponent>')
+def gameStats(opponent):
+	
+	conn = psycopg2.connect(
+	host="localhost",
+	port=5432,
+	database="chend2",
+	user="chend2",
+	password="plad242books")
+
+	cur = conn.cursor()
+
+  	sql_GamePoints = """SELECT * FROM cutstats WHERE Opponent = %s ORDER BY Point DESC;"""\
+	cur.execute(sql_GamePoints)
+  	cornellHucks = cur.fetchall()
 	return render_template("homepage.html", someText = f"hello")
 
 if __name__ == '__main__':
