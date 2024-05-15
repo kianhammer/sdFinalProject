8679c5cd0b1c061ca60bda58a1d233301a82ab00
DanielChen29 <daniel.yp.chen@gmail.com>
2024-05-15 10:23:35 -0500

    homepage

--- website/pages.py
+++ website/pages.py
@@ -1,13 +1,30 @@
 from flask import Flask
 from flask import render_template
-import random
+import psycopg2
 
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,   
+        database="chend2",
+        user="chend2",
+        password="plad242books")
+
+    cur = conn.cursor()
+
+    sql = f"SELECT Opponent FROM CUTStats"
+    
+    cur.execute( sql )
+
+    rows = cur.fetchall()
+
+    gameset = set(rows)
+
     message = f"CUT stats web page" 
-    return render_template("homepage.html", someText = message)
+    return render_template("homepage.html", gamenumber = len(gameset))
 
 if __name__ == '__main__':
     my_port = 5202
