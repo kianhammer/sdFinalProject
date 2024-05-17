5f3d57e9bae87c0b415da91865d437ad2cf9f3b4
kianhammer <165401427+kianhammer@users.noreply.github.com>
2024-05-17 11:09:21 -0500

    Update pages.py

--- website/pages.py
+++ website/pages.py
@@ -42,7 +42,7 @@ def gameStats(opponent):
 	cur = conn.cursor()
 
 	sql_game_points = """SELECT * FROM cutstats WHERE Opponent = %s ORDER BY Point DESC;"""
-	cur.execute(sql_game_points, "Cornell")
+	cur.execute(sql_game_points, opponent)
 	game_points = cur.fetchall()
 	return render_template("homepage.html", some_text = f"hello")
 
