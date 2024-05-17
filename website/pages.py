fa44c3bd395a6b4bd8272d080ea6433935daea34
JulianRW1 <julianrwalston@gmail.com>
2024-05-16 22:01:07 -0500

    added playerstats page

--- website/pages.py
+++ website/pages.py
@@ -9,6 +9,10 @@ def welcome():
     message = f"CUT stats web page" 
     return render_template("homepage.html", someText = message)
 
+@app.route('/stats/players')
+def player_stats():
+    return render_template("playerStats.html")
+
 if __name__ == '__main__':
     my_port = 5202
     app.run(host='0.0.0.0', port = my_port) 
