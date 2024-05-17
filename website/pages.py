f9f464804f20fc33504e27dcd74c13f5bca93232
kianhammer <165401427+kianhammer@users.noreply.github.com>
2024-05-17 11:03:17 -0500

    Update pages.py

--- website/pages.py
+++ website/pages.py
@@ -6,28 +6,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    conn = psycopg2.connect(
+	conn = psycopg2.connect(
         host="localhost",
         port=5432,   
         database="chend2",
         user="chend2",
         password="plad242books")
 
-    cur = conn.cursor()
+	cur = conn.cursor()
 
-    sql = f"SELECT Opponent FROM CUTStats"
+	sql = f"SELECT Opponent FROM CUTStats"
     
-    cur.execute( sql )
+	cur.execute( sql )
 
-    rows = cur.fetchall()
+	rows = cur.fetchall()
 
-    gameset = set(rows)
+	gameset = set(rows)
 
-    return render_template("homepage.html", gamenumber = len(gameset))
+	return render_template("homepage.html", gamenumber = len(gameset))
 
 @app.route('/import/')
 def importpage():
-    return render_template("importgame.html")
+	return render_template("importgame.html")
 
 @app.route('/stats/game/<opponent>')
 def gameStats(opponent):
@@ -47,5 +47,5 @@ def gameStats(opponent):
 	return render_template("homepage.html", someText = f"hello")
 
 if __name__ == '__main__':
-    my_port = 5202
-    app.run(host='0.0.0.0', port = my_port) 
+	my_port = 5202
+	app.run(host='0.0.0.0', port = my_port) 
