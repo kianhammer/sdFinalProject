6b4bd65f3b95bdea23f70b000530a0e43c9ad409
kianhammer <165401427+kianhammer@users.noreply.github.com>
2024-05-18 15:17:24 -0500

    Update gamestats.js

--- website/static/gamestats.js
+++ website/static/gamestats.js
@@ -1,1 +1,22 @@
+function updateGame(data) {
+    the_json = data;
 
+    score = the_json['score'];
+    
+    the_answer = document.getElementById("game_stats_display");
+    the_answer.innerHTML = "The game score: " + score; 
+
+}
+
+function fetchGame() {
+    the_game = document.getElementById("game_input").value;
+
+    URL = "/game/stats/" + the_game;
+
+    fetch(URL).then( response => response.json()).then( data => updateGame(data) );
+
+
+    
+    console.log(URL);
+   
+}
