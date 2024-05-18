757763a2e38923848a1d23d40f0f5ff290eb7966
JulianRW1 <julianrwalston@gmail.com>
2024-05-18 10:09:28 -0500

    debugging 2d array json transfer

--- website/static/playerStats.js
+++ website/static/playerStats.js
@@ -17,8 +17,9 @@ function addDataToTable(playerData) {
     for(var i = 0; i < playerData.length; i++)
         var row = table.insertRow(i+1); // skip over header
         console.log("row = " + playerData[i])
-        for(var j = 0; j < playerData[i].length; j++) {
+        for(var j = 0; j < 3; j++) {
             var cell = row.insertCell(j);
+            console.log("playerData[i][j] = " + playerData[i][j])
             cell.innerHTML = playerData[i][j]
         }
 
