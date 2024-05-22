3ff7fa46a49c8934b82d65939c83933213ba34ff
kianhammer <165401427+kianhammer@users.noreply.github.com>
2024-05-22 10:52:38 -0500

    Update gamestats.js

--- website/static/gamestats.js
+++ website/static/gamestats.js
@@ -11,6 +11,8 @@ function updateGame(data) {
 function fetchGame() {
     the_game = document.getElementById("change_game_input").value;
 
+    console.log(the_game);
+    
     URL = "/stats/game/" + the_game;
 
     fetch(URL).then( response => response.json()).then( data => updateGame(data) );
