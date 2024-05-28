abe1b857d5c9ae9f5f457acc7132c080003512ae
kianhammer <165401427+kianhammer@users.noreply.github.com>
2024-05-28 17:17:43 -0500

    Update gamestats.js

--- website/static/gamestats.js
+++ website/static/gamestats.js
@@ -1,32 +1,11 @@
-function updateGame(data) {
-    the_json = data;
-    
+function updateGame(data) {      
     statsDisplay = "";
     for (var key in data){
         statsDisplay = statsDisplay + "The game " + key + ": " + data[key] + "<br>";
     }
 
     the_answer = document.getElementById("game_stats_display");
-    the_answer.innerHTML = statsDisplay; 
-    
-    score = the_json['score'];
-    opponent = the_json['opponent'];
-    date = the_json['date'];
-    cutBreaks = the_json['cutBreaks'];
-    cutHolds = the_json['cutHolds'];
-    oppBreaks = the_json['oppBreaks'];
-    oppHolds = the_json['oppHolds'];
-    completeHucks = the_json['completeHucks'];
-    incompleteHucks = the_json['incompleteHucks'];
-    endzoneScores = the_json['endzoneScores'];
-    endzoneNotScores = the_json['endzoneNotScores'];
-    blocksForced = the_json['blocksForced'];
-    blocksUnforced = the_json['blocksUnforced'];
-    turnoversForced = the_json['turnoversForced'];
-    turnoversUnforced = the_json['turnoversUnforced'];
-    
-    
-
+    the_answer.innerHTML = statsDisplay;  
 }
 
 
