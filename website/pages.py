ece6291440efe3c78aaca0a3362e8bfab31b78f4
kianhammer <165401427+kianhammer@users.noreply.github.com>
2024-05-29 10:56:49 -0500

    Update pages.py

--- website/pages.py
+++ website/pages.py
@@ -69,10 +69,9 @@ def separate_games(all_points):
 	for point in all_points:
 		if not game:
 			game.append(point)
-		#if the current point is listed as the first point of the game, the previous points appended to game were all of the previous game
 		else:
-			if:
-				point[3] == 1:
+			#if the current point is listed as the first point of the game, the previous points appended to game were all of the previous game
+			ifpoint[3] == 1:
 				all_cut_games.append(game)
 				game = []
 				game.append(point)
