26c7c56f534563d688f2d7d04010dfdf41f36ffb
JulianRW1 <julianrwalston@gmail.com>
2024-06-01 21:32:01 -0500

    change home button in header to image

--- website/static/header.js
+++ website/static/header.js
@@ -13,39 +13,47 @@ class Header extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
         <style>
-          ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-            background-color: #333;
-          }
-          
-          li {
-            float: left;
-          }
-          
-          li a {
-            display: block;
-            color: white;
-            text-align: center;
-            padding: 14px 16px;
-            text-decoration: none;
-          }
-          
-          /* Change the link color to #111 (black) on hover */
-          li a:hover {
-            background-color: #111;
-          }
-        </style>
-        <header>
-          <ul>
-            <li><a href="/">CUTRULES</a></li>
-            <li><a href="/stats/players">Player Stats</a></li>
-            <li><a href="/stats/game">Game Stats</a></li>
-            <li><a href="/import/">Import</a></li>
-          </ul>
-        </header>
+        ul {
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+          overflow: hidden;
+          background-color: #333;
+        }
+        
+        li {
+          float: left;
+        }
+        
+        li a {
+          display: block;
+          color: white;
+          text-align: center;
+          text-decoration: none;
+        }
+        
+        .txt {
+          padding: 14px 16px;
+        }
+        .img {
+          padding: 10px 16px;
+        }
+        
+        /* Change the link color to #111 (black) on hover */
+        li:hover {
+          background-color: #111;
+        }
+      </style>
+      <header>
+        <ul>
+          <li><a class="img" href="/">
+            <img src="https://cdn.carleton.edu/uploads/sites/378/2023/09/perfect-cut-logo.png?resize=20,20" alt="CUTRULES">
+          </a></li>
+          <li><a class="txt" href="/stats/players">Player Stats</a></li>
+          <li><a class="txt" href="/stats/game">Game Stats</a></li>
+          <li><a class="txt" href="/import/">Import</a></li>
+        </ul>
+      </header>
       `;
     }
 }
