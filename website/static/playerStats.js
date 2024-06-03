c35f4eae29cd9eddde572c53640c9b3a325f1154
JulianRW1 <julianrwalston@gmail.com>
2024-06-03 11:00:57 -0500

    try keeping rightmost tooltip on the page v5

--- website/static/playerStats.js
+++ website/static/playerStats.js
@@ -33,7 +33,7 @@ function populateStatsTableHeader(statCategories, tooltips) {
   var th = tableHeaderRow.getElementsByTagName("TH")[index]; // the last th
   console.log("span: " + th.getElementsByTagName("p")[0].innerHTML);
   span = th.getElementsByTagName("SPAN")[0];
-  span.style.left = "-50";
+  span.style.left = "-50px";
   span.style.margin = "0 60px 0 0";
 }
 
